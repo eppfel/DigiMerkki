76eb1d3222dfad0f7c0db36d1bbcbe896874c4ae
Felix A. Epp <work@felixepp.de>
2021-01-28 18:36:43 +0200

    Fix display setup for TFT_eSPI library

--- include/tdisplay-v3.h
+++ include/tdisplay-v3.h
@@ -1,26 +1,3 @@
-#define USER_SETUP_LOADED 1
-#define ST7789_DRIVER 1
-#define TFT_WIDTH 135
-#define TFT_HEIGHT 240
-#define CGRAM_OFFSET 1
-#define TFT_MOSI 19
-#define TFT_SCLK 18
-#define TFT_CS 5
-#define TFT_DC 16
-#define TFT_RST 23
-#define TFT_BL 4
-#define TFT_BACKLIGHT_ON HIGH
-#define LOAD_GLCD 1
-#define LOAD_FONT2 1
-#define LOAD_FONT4 1
-#define LOAD_FONT6 1
-#define LOAD_FONT7 1
-#define LOAD_FONT8 1
-#define LOAD_GFXFF 1
-#define SMOOTH_FONT 1
-#define SPI_FREQUENCY 40000000
-#define SPI_READ_FREQUENCY 6000000 // 6 MHz is the maximum SPI read speed for the ST7789V
-
 #define TOUCHPIN_LEFT 13       // Pin for sensing touch input A
 #define TOUCHPIN_RIGHT 2      // Pin for sensing touch input B
 #define DEBOUNCE_TIME 50
--- include/tdisplay.h
+++ include/tdisplay.h
@@ -1,26 +1,3 @@
-#define USER_SETUP_LOADED 1
-#define ST7789_DRIVER 1
-#define TFT_WIDTH 135
-#define TFT_HEIGHT 240
-#define CGRAM_OFFSET 1
-#define TFT_MOSI 19
-#define TFT_SCLK 18
-#define TFT_CS 5
-#define TFT_DC 16
-#define TFT_RST 23
-#define TFT_BL 4
-#define TFT_BACKLIGHT_ON HIGH
-#define LOAD_GLCD 1
-#define LOAD_FONT2 1
-#define LOAD_FONT4 1
-#define LOAD_FONT6 1
-#define LOAD_FONT7 1
-#define LOAD_FONT8 1
-#define LOAD_GFXFF 1
-#define SMOOTH_FONT 1
-#define SPI_FREQUENCY 40000000
-#define SPI_READ_FREQUENCY 6000000 // 6 MHz is the maximum SPI read speed for the ST7789V
-
 #define TOUCHPIN_LEFT T7       // Pin for sensing touch input 27
 #define TOUCHPIN_RIGHT T8      // Pin for sensing touch input 33 (labelled as 32)
 #define DEBOUNCE_TIME 35
