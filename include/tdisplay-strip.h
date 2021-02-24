29a7d8d2279c1f9386bdce92ab5ed0c58e4ad03d
Felix A. Epp <felix.epp@aalto.fi>
2021-02-24 12:26:11 +0200

    Add support for strip with 7 light emitting diodes

---
+++ include/tdisplay-strip.h
@@ -0,0 +1,12 @@
+#define TOUCHPIN_LEFT 13       // Pin for sensing touch input A
+#define TOUCHPIN_RIGHT 15      // Pin for sensing touch input B
+#define DEBOUNCE_TIME 40
+#define TTHRESHOLD 60     // threshold for touch
+#define STHRESHOLD 20     // threshold for wake up touch
+#define HW_BUTTON_PIN1 35 // Hardware button 1 on the T-display board
+#define HW_BUTTON_PIN2 0  // Hardware button 1 on the T-display board
+
+#define ADC_PIN 34
+
+#define NUM_LEDS 7 // Number of LEDs conrolled through FastLED
+#define NEOPIXEL_PIN 12
