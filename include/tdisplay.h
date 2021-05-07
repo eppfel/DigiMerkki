8e22d223f2155cb5d15db2db5d78d71920cbed6f
Felix A. Epp <felix.epp@aalto.fi>
2021-05-07 14:07:37 +0300

    Remove obsolete board default headers

--- include/tdisplay.h
+++ include/tdisplay.h
@@ -1,12 +1,13 @@
-#define TOUCHPIN_LEFT T7       // Pin for sensing touch input 27
-#define TOUCHPIN_RIGHT T8      // Pin for sensing touch input 33 (labelled as 32)
-#define DEBOUNCE_TIME 35
-#define TTHRESHOLD 40     // threshold for touch
-#define STHRESHOLD 20     // threshold for wake up touch
+#define TOUCHPIN_LEFT 13  // Pin for sensing touch input A
+#define TOUCHPIN_RIGHT 15 // Pin for sensing touch input B
+#define DEBOUNCE_TIME 40
+#define TTHRESHOLD 60     // threshold for touch
+#define STHRESHOLD 30     // threshold for wake up touch
 #define HW_BUTTON_PIN1 35 // Hardware button 1 on the T-display board
 #define HW_BUTTON_PIN2 0  // Hardware button 1 on the T-display board
 
+#define ADC_EN 14 //ADC_EN is the ADC detection enable port
 #define ADC_PIN 34
 
 #define NUM_LEDS 5 // Number of LEDs conrolled through FastLED
-#define NEOPIXEL_PIN 26
+#define NEOPIXEL_PIN 12
